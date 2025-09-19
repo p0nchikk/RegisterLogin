bd70cbc0ec3e8380cbbbc644e0efd595480cf818
Polinka Prokopenko <prokopenkopolinka@outlook.com>
2025-09-19 21:23:16 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RegisterLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
